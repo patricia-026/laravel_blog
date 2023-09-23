fabe18496095d9e8d977b6e43de5328c11b5a93b
Patricia <kovach.patri@gmail.com>
2023-09-23 13:05:55 +0200

    Section 7 - filtering

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,8 +21,13 @@ class Post extends Model
             ->orWhere('body', 'like', '%' . $search . '%'));
 
 
-        if ($filters['search'] ?? false) {
-        }
+        $query->when($filters['category'] ?? false, fn ($query, $category) =>
+        $query->whereHas('category', fn ($query) =>
+        $query->where('slug', $category)));
+
+        $query->when($filters['author'] ?? false, fn ($query, $author) =>
+        $query->whereHas('author', fn ($query)=> $query->where('username', $author)));
+
 
         return $query;
     }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    @include('_posts-header')
+    @include('posts._header')
 
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
@@ -9,11 +9,6 @@
             <p class="text-center">No posts yet. Please check back later</p>
         @endif
 
-        {{-- <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div> --}}
     </main>
 
 
