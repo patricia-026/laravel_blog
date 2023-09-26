cdeb445a9ccd5c304ee669668c6aa50d6aaed206
Patricia <kovach.patri@gmail.com>
2023-09-26 10:05:33 +0200

    Section 11 - newsletters and APIs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\Newsletter;
+use App\Services\MailchimpNewsletter;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us17'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,26 @@
 <?php
 
-use App\Http\Controllers\CommentController;
+use App\Models\Post;
+use App\Models\User;
+use Mockery\Exception;
+use App\Models\Category;
+use App\Services\Newsletter;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-use App\Models\Category;
-use App\Models\User;
-use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Support\Facades\File;
+use App\Http\Controllers\NewsletterController;
+use Illuminate\Validation\ValidationException;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [CommentController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
